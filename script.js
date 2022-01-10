b5f557171e00f45feb0f9dede17ebfec34a7b6bf
Kirill Yerenkoff <32268957+yerenkoff@users.noreply.github.com>
2022-01-10 17:52:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,22 @@
 // setTimeout(function(){
 // }, 1000)
 let header = document.getElementsByTagName("header")[0];
+
+
 function bodyScroll() {
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     if (window.scrollY > 100) {
         header.style.height = "50px";
     } else {
         header.style.height = "100px";
     }
+}
+
+
+
+function counterChange(arrow, sign){
+    let currentAmount = arrow.parentElement.previousElementSibling.value
+    let currentInput = arrow.parentElement.previousElementSibling;
+    currentInput.value = eval(currentAmount + sign + 1) < 0 ? 0 : eval(currentAmount + sign + 1);
+    console.log(currentAmount);
 }
